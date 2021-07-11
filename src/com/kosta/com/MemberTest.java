c42181cbb78d1a81a6ec311671b927f6d003df3a
anko <gantri300@gmail.com>
2021-07-11 16:25:10 +0900

    제출

--- src/com/kosta/com/MemberTest.java
+++ src/com/kosta/com/MemberTest.java
@@ -6,20 +6,19 @@ public class MemberTest {
 	public static void main(String[] args) {
 		MemberDAO dao = new MemberDAO();
 		Scanner scanner = new Scanner(System.in);
+		String id;
 		
 		while(true) {
-			
 			System.out.println("=======================================================");
 			System.out.println("Menu\n1.회원가입\t 2.회원수정\t 3.회원삭제\t 4.회원보기\t 5.전체조회\t 6.종료");
 			System.out.println("=======================================================");
 			System.out.println(" 원하는 메뉴를 입력하세요 " );
 			String choice = scanner.nextLine();
-					
 
 			if(choice.trim().equals("1")) {
 				System.out.println("1. 회원가입");
 				System.out.println("원하는 ID를 입력하세요");
-				String id = scanner.nextLine();
+				id = scanner.nextLine();
 
 				System.out.println("원하는 비밀번호를 입력하세요");
 				String pw = scanner.nextLine();
@@ -41,7 +40,7 @@ public class MemberTest {
 			else if(choice.trim().equals("2")){
 				System.out.println("2. 회원수정");
 				System.out.println("수정할 ID를 입력하세요");
-				String id = scanner.nextLine();
+				id = scanner.nextLine();
 				int result = dao.update(id, scanner);
 				if(result>=1)
 					System.out.println("수정 완료");
@@ -52,7 +51,7 @@ public class MemberTest {
 			else if (choice.trim().equals("3")) {
 				System.out.println("3.회원삭제");
 				System.out.println("삭제할 ID를 입력하세요 ");
-				String id = scanner.nextLine();
+				id = scanner.nextLine();
 				int result = dao.delete(id);
 				if(result >=1) 
 					System.out.println("삭제완료");
@@ -63,7 +62,7 @@ public class MemberTest {
 			else if(choice.trim().equals("4")) {
 				System.out.println("4. 회원보기");
 				System.out.println("조회할 ID를 입력하세요");
-				String id = scanner.nextLine();
+				id = scanner.nextLine();
 				dao.read(id, scanner);
 			}
 			
